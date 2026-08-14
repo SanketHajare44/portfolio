92a854d8fbe17ba98b98f2050fd0af81907d2d94
SanketHajare44 <sankethajare44@gmail.com>
2026-08-14 09:23:09 +0530

    fix: scroll to top on route change

--- client/src/components/layout/Layout.jsx
+++ client/src/components/layout/Layout.jsx
@@ -3,10 +3,12 @@ import { Outlet } from "react-router-dom";
 import CommandPalette from "../ui/CommandPalette";
 import Footer from "./Footer";
 import Navbar from "./NavBar";
+import ScrollToTop from "../ui/ScrollToTop";
 
 const Layout = () => {
     return (
         <>
+            <ScrollToTop />
             <Navbar />
 
             <CommandPalette />
